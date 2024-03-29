b7643e6c34ffca4d7c67d21bea547769c2703ed7
Shovgur <s.showgur@yandex.by>
2024-03-29 18:35:38 +0300

    fix: add lucky modal

---
+++ src/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <div className="text-center pt-12">
+      <button className="button inline-block relative text-[20px] ">Continue</button>
+    </div>
+  );
+};
+
+export default Button;
--- src/Luck.tsx
+++ src/Luck.tsx
@@ -1,17 +1,31 @@
-import { Button } from "./components/ui/button";
+import Button from "./Button";
 
 const Luck = () => {
   return (
-    <div className="bg-[url('../public/LuckBackground.png')] bg-cover max-w-[628px] max-h-[326px]">
-      <div className="pt-14 flex flex-col items-center gap-8 ">
-        <img
-          className="px-[91px]"
-          src="../public/Try-your_luck.png"
-          alt="luck"
-        />
-        <p className="text-white">Get a great chance to win a valuable prize</p>
+    <div className="bg-[url('../public/LuckBackground.png')] bg-cover lg:max-w-[628px] lg:h-[326px] relative">
+      <div className="flex flex-col items-center">
+        <div className="pt-14 flex flex-col items-center gap-8 ">
+          <img
+            className="px-[91px]"
+            src="../public/Try-your_luck.png"
+            alt="luck"
+          />
+          <p className="text-white">
+            Get a great chance to win a valuable prize
+          </p>
+        </div>
+        <Button />
       </div>
-      <Button />
+      <img
+        className="absolute lg:top-[77px] lg:left-[-38px]"
+        src="/public/Item_perk_1.svg"
+        alt="perk_1"
+      />
+      <img
+        className="absolute lg:right-[22px] lg:top-[268px]"
+        src="/public/Item_perk_2.svg"
+        alt="perk_2"
+      />
     </div>
   );
 };
