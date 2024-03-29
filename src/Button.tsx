312956b086296511769af2571fed9fbf80ce6096
Pavel-Surkov <surkovv.pavel@gmail.com>
2024-03-30 02:33:03 +0700

    fix: Continue button

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,7 +1,22 @@
 const Button = () => {
   return (
     <div className="text-center pt-12">
-      <button className="button inline-block relative text-[20px] ">Continue</button>
+      <button className="inline-block relative text-[26px] text-white w-[368px] h-[98px]">
+        <div className="skewed absolute left-0 top-0 w-full h-full">
+          <div className="absolute left-0 top-0 w-full h-full bg-gradient-gold p-1 rounded-[14px]" />
+          <div className="bg-gradient-green rounded-[10px] py-3 px-5 absolute left-1/2 -translate-x-1/2 -translate-y-1/2 top-1/2 w-[calc(100%-8px)] h-[calc(100%-8px)]" />
+        </div>
+        <img
+          className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-[calc(100%+10px)] h-[calc(100%-16px)] aspect-[372/82] max-w-none"
+          src="/btn-bg.png"
+          width={380}
+          height={82}
+          alt=""
+        />
+        <span className="relative inline-block py-4 px-8 text-shadow">
+          Continue
+        </span>
+      </button>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
-  prefix: "",
+  prefix: '',
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
+      backgroundImage: {
+        'gradient-gold':
+          'radial-gradient(45.88% 297.28% at 45.88% 49.32%, #FBF298 0%, rgba(251, 242, 152, 0) 100%), linear-gradient(90deg, #C08434 0%, #BC7D2D 91.75%)',
+        'gradient-green':
+          'radial-gradient(45.88% 297.28% at 45.88% 50%, #6DD767 0%, rgba(109, 215, 103, 0) 92.71%), linear-gradient(90deg, #3D882F 0%, #2A6326 91.75%)',
+      },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [require('tailwindcss-animate')],
+};
