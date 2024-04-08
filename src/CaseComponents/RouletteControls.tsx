473b7c9540ad4dd9bb10975a55f4b88831fcd408
Shovgur <s.showgur@yandex.by>
2024-04-08 18:44:12 +0300

    fix: little update

--- src/CaseComponents/RouletteControls.tsx
+++ src/CaseComponents/RouletteControls.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import cl from "./roulette.module.scss";
-import back_arrow from "../../public/icons/back_arrow.svg";
-import setting_icon from "../../public/icons/setting_icon.svg";
-import info from "../../public/icons/info.svg";
-import spinner from "../../public/icons/spinner-refresh.svg";
-import spin from "../../public/spin_case.svg";
+import back_arrow from "../../public/icons/back_arrow.png";
+import setting_icon from "../../public/icons/setting_button.png";
+import info from "../../public/icons/info_button.png";
+import spinner from "../../public/icons/spinner_refresh.png";
+import spin from "../../public/icons/spin_case.png";
 import controls_bg from "../../public/controls_case_background.png";
 
 interface RouletteControlsProps {
@@ -22,23 +22,23 @@ const RouletteControls: React.FC<RouletteControlsProps> = ({
       className="w-[828px] lg:mt-[82px] relative bg-cover h-[200px]"
     >
       <div className="absolute top-1/2 -translate-y-1/2 lg:left-[90px] flex gap-6">
-        <button className="border rounded-[20px] p-3">
+        <button className="w-[52px] h-[52px]">
           <img src={back_arrow} alt="back_arrow" />
         </button>
-        <button className="border rounded-[20px] p-3">
+        <button className="w-[52px] h-[52px]">
           <img src={setting_icon} alt="setting_icon" />
         </button>
-        <button className="border rounded-[20px] p-3">
+        <button className="w-[52px] h-[52px]">
           <img src={info} alt="info_icon" />
         </button>
       </div>
       <div className="absolute top-1/2 -translate-y-1/2 lg:right-[90px] flex gap-6">
-        <button className="border rounded-[20px] p-3">
+        <button className="w-[52px] h-[52px]">
           <img src={spinner} alt="info_icon" />
         </button>
       </div>
       <button className={cl.button} disabled={disabled} onClick={onSpin}>
-        <img src={spin} alt="Spin" />
+        <img className="w-[102px] h-[102px]" src={spin} alt="Spin" />
       </button>
     </div>
   );
