b12330ba7ad843fd09a30a7c85e0f54891583f39
Shovgur <s.showgur@yandex.by>
2024-04-10 11:09:41 +0300

    fix: mobile roulette controls

--- src/CaseComponents/RouletteControls.tsx
+++ src/CaseComponents/RouletteControls.tsx
@@ -5,7 +5,8 @@ import setting_icon from "../../public/icons/setting_button.png";
 import info from "../../public/icons/info_button.png";
 import spinner from "../../public/icons/spinner_refresh.png";
 import spin from "../../public/icons/spin_case.png";
-import controls_bg from "../../public/controls_case_background.png";
+import desktopBg from "../../public/controls_case_background.png";
+import mobileBg from "../../public/controls_bg_mob.png";
 
 interface RouletteControlsProps {
   onSpin: () => void;
@@ -16,12 +17,13 @@ const RouletteControls: React.FC<RouletteControlsProps> = ({
   onSpin,
   disabled,
 }) => {
+  const backgroundImg = window.innerWidth <= 768 ? mobileBg : desktopBg;
   return (
     <div
-      style={{ backgroundImage: `url(${controls_bg})` }}
+      style={{ backgroundImage: `url(${backgroundImg})` }}
       className="lg:w-[828px] w-[390px] lg:mt-[82px] relative bg-cover h-[123px] lg:h-[200px]"
     >
-      <div className="absolute top-1/2 -translate-y-1/2 lg:left-[90px] flex gap-4 lg:gap-6">
+      <div className="absolute left-7 top-[60%] lg:top-1/2 -translate-y-1/2 lg:left-[90px] flex gap-4 lg:gap-6">
         <button className=" w-[40px] h-[40px] lg:w-[52px]  lg:h-[52px] lg:block hidden">
           <img src={back_arrow} alt="back_arrow" />
         </button>
@@ -32,8 +34,8 @@ const RouletteControls: React.FC<RouletteControlsProps> = ({
           <img src={info} alt="info_icon" />
         </button>
       </div>
-      <div className="absolute top-1/2 -translate-y-1/2 lg:right-[90px] flex gap-6">
-        <button className=" w-[40px] h-[40px] lg:w-[52px]   lg:h-[52px]">
+      <div className="absolute top-[60%] lg:top-1/2 -translate-y-1/2 lg:right-[90px] right-7 flex gap-6">
+        <button className=" w-[40px] h-[40px] lg:w-[52px] lg:h-[52px]">
           <img src={spinner} alt="info_icon" />
         </button>
       </div>
